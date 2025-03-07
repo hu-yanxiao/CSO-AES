6644a9f76d1848129db19eb472979359cbdaeff7
huang-jing <137173735+dream123321@users.noreply.github.com>
2025-03-07 11:30:57 +0800

    Add files via upload

--- CSO-AES/cso_aes/cso_aes_das/vasp_main_xyz.py
+++ CSO-AES/cso_aes/cso_aes_das/vasp_main_xyz.py
@@ -4,6 +4,8 @@ from .no_success_bsub import no_success_bsub
 from ase.io import iread, write, read
 from tqdm import tqdm
 import numpy as np
+import re
+from pathlib import Path
 
 
 def remove(file):
@@ -17,8 +19,45 @@ def ok(path):
         t = 1
     return t
 
+def INCAR_NELM(INCAR):
+    # 正则表达式匹配 NELM 的值
+    pattern = re.compile(r"NELM\s*=\s*(\d+)")
+
+    # 打开文件并提取 NELM 的值
+    nelm_value = 60
+    with open(INCAR, "r") as file:
+        for line in file:
+            match = pattern.search(line)
+            #print(match)
+            if match:
+                nelm_value = int(match.group(1))
+    return nelm_value
+
+def nmax_label(logout,nmax):
+    with open(logout, 'r') as file:
+        for line in file:
+            if 'DAV:' in line:
+                string = line.split()
+        log_nmax = int(string[1])
+    label = True
+    #print(log_nmax)
+    if int(log_nmax) == nmax:
+        label = False
+    return label
+
+
+def get_parent_directory(target_dir, levels=1):
+    """获取指定目录的上级目录"""
+    path = Path(target_dir)
+    for _ in range(levels):
+        path = path.parent
+    return path
+
 def vasp_main_xyz(current, out_name, ori_out_name, force_threshold):
     dirs = [file for file in os.listdir(current) if os.path.isdir(os.path.join(current,file)) and file != '__pycache__']
+    INCAR_path = os.path.join(get_parent_directory(current, levels=5), 'init', 'INCAR')
+    nmax = INCAR_NELM(INCAR_path)
+    #print(nmax)
     remove(out_name)
     remove(ori_out_name)
     ok_count = 0
@@ -34,9 +73,10 @@ def vasp_main_xyz(current, out_name, ori_out_name, force_threshold):
             try:
                 ok_count = ok_count + ok(sub_dir_path)
                 if ok(sub_dir_path)==1:
-                    atom = collect_efs(sub_dir_path)
-                    write(ori_out_name, atom, format='extxyz', append=True)
-                    len_count = len_count + 1
+                    if nmax_label(os.path.join(sub_dir_path,'logout'), nmax):
+                        atom = collect_efs(sub_dir_path)
+                        write(ori_out_name, atom, format='extxyz', append=True)
+                        len_count = len_count + 1
             except:
                 server = None #(废弃的功能)
                 no_success_bsub(server, sub_dir_path)
@@ -63,9 +103,9 @@ def vasp_main_xyz(current, out_name, ori_out_name, force_threshold):
 
 if __name__ =='__main__':
     pwd = os.getcwd()
-    current = os.path.join(pwd,'scf','filter')
-    out_name =os.path.join(pwd,'out.xyz')
-    ori_out_name = os.path.join(pwd, 'ori_out.xyz')
+    current = os.path.join(pwd, 'scf_lammps_data', 'scf', 'filter')
+    out_name = os.path.join(pwd, 'scf_lammps_data', 'scf_filter.xyz')
+    ori_out_name = os.path.join(pwd, 'scf_lammps_data', 'ori_scf_filter.xyz')
     force_threshold = 10
-    ok_count,len_count,no_success_bsub_path, force_count = vasp_main_xyz(current, out_name, ori_out_name, force_threshold)
-    print(ok_count,len_count,no_success_bsub_path, force_count)
+    ok_count, len_count, no_success_bsub_path, force_count, force_of_force_count_0 = vasp_main_xyz(current, out_name, ori_out_name, force_threshold)
+    print(ok_count, len_count, no_success_bsub_path, force_count)
